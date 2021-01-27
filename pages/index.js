353de0d79a412797f86d62b26c9b959eb8d14189
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-01-26 22:05:38 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,29 @@ const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
 `
+const Background = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  flex: 1;
+`
+
+const Form = styled.div`
+  width: 100%;
+  max-width: 350px;
+  background-color: ${({ theme }) => theme.colors.primary};
+`
 
 export default function Home() {
   return (
-    <Title>My page</Title>
+    <Background>
+      <Form>
+        adsdsa
+      </Form>      
+    </Background>
+
   )
 }
