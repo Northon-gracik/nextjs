8bdd3b9c660e7b8625b07f6868c704d5341f534d
Northon-gracik <72635231+Northon-gracik@users.noreply.github.com>
2021-02-01 21:42:01 -0300

    Update index.js

--- src/components/QuizBackground/index.js
+++ src/components/QuizBackground/index.js
@@ -9,7 +9,7 @@ const QuizBackground = styled.div`
   background-color: ${({ theme }) => theme.colors.mainBg || "#00f"};
   flex: 1;
   @media screen and (max-width: 500px) {
-    background-image: none;
+    background-image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Flinguinecode.com%2Fpost%2Fdeploy-nextjs-custom-server-to-now-2&psig=AOvVaw09Iuhie2oFxm8a2FrjPaIY&ust=1611876845456000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDBkqmjve4CFQAAAAAdAAAAABAh";
     &:after {
       content: "";
       background-size: cover;
